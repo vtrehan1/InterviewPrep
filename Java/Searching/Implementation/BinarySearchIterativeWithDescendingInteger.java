53b1818e984fdd23188e6af9ab939d62aad65c35
PranavRudra <pranav1024@gmail.com>
2018-01-03 11:22:17 -0700

    Implemented an iterative binary search for integer arrays sorted in descending order

---
+++ Java/Searching/Implementation/BinarySearchIterativeWithDescendingInteger.java
@@ -0,0 +1,27 @@
+//Implementation of iterative binary search on an array of integers sorted in descending order
+
+public class BinarySearchIterativeDescendingInteger {
+	
+	//Returns index of value to find or -1 if it cannot be found
+	public static int binarySearch(int[] arr, int value) {
+		if (arr.length == 0)
+			return -1;
+		
+		int low = arr.length-1;
+		int high = 0;
+		int midpoint = -1;
+
+		while(true) {
+			if (high == low)
+				break;
+			midpoint = (low+high)/2;
+			if (arr[midpoint] == value)
+				return midpoint;
+			if (arr[midpoint] > value)
+				low = midpoint-1;
+			else
+				high = midpoint+1;
+		}
+		return -1;
+	}
+}
