fea31e56d4864597c17315c24e79e82fa4a802c2
PranavRudra <pranav1024@gmail.com>
2018-01-03 11:53:14 -0700

    Added a recursive binary search implementation for an integer array sorted in descending order

---
+++ Java/Searching/Implementation/src/BinarySearchRecursiveWithDescendingInteger.java
@@ -0,0 +1,27 @@
+//Implementation of recursive binary search on an array of integers sorted in descending order
+
+public class BinarySearchRecursiveDescendingInteger {
+
+	public static int binarySearch(int[] arr, int value) {
+		if (arr.length == 0)
+			return -1;
+		
+		int low = arr.length-1;
+		int high = 0;
+		int midpoint = -1;
+		
+		return findElement(arr, value, low, high, midpoint);
+	}
+
+	private static int findElement(int[] arr, int value, int low, int high, int midpoint) {
+		if (high == low)
+			return -1;
+		if (arr[midpoint] == value)
+			return midpoint;
+		if (arr[midpoint] > value)
+			low = midpoint - 1;
+		else 
+			high = midpoint + 1;
+		return findElement(arr,value,low,high,midpoint);
+	}
+}
