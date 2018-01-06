b24e4eba9376c41b4b0f6305aef08be4d54b4ba7
PranavRudra <pranav1024@gmail.com>
2018-01-05 18:13:52 -0700

    Formatted array implementation code

--- Java/Arrays/Implementation/src/DynamicArray.java
+++ Java/Arrays/Implementation/src/DynamicArray.java
@@ -46,7 +46,7 @@ public class DynamicArray {
     //Updates value at a particular index
     //Blows up if that index doesn't already have a value
     public void update(int index, Object item) {
-    		if (index < 0 || index >= this.size)
+    	if (index < 0 || index >= this.size)
             throw new ArrayIndexOutOfBoundsException();
         this.arr[index] = item;
     }
@@ -62,61 +62,61 @@ public class DynamicArray {
     //Inserts the item at the specified index
     //Trailing indices are shifted right
     public void insert(int index, Object item) {
-    		if (index < 0 || (index >= this.size && index != 0))
-            throw new ArrayIndexOutOfBoundsException();
-    		if (this.size == this.capacity)
-    			resize(2*this.capacity);
-    		for (int i=this.size; i>index; i--)
-    			this.arr[i] = this.arr[i-1];
-    		this.arr[index] = item;
-    		this.size++;
+        if (index < 0 || (index >= this.size && index != 0))
+        throw new ArrayIndexOutOfBoundsException();
+        if (this.size == this.capacity)
+            resize(2*this.capacity);
+        for (int i=this.size; i>index; i--)
+            this.arr[i] = this.arr[i-1];
+        this.arr[index] = item;
+        this.size++;
     }
     
     //Inserts the item at index 0
     public void prepend(Object item) {
-    		insert(0,item);
+        insert(0,item);
     }
     
     //Deletes element at the index
     //Shifts all trailing elements left
     public void delete(int index) {
-    		if (index < 0 || index >= this.size)
+        if (index < 0 || index >= this.size)
             throw new ArrayIndexOutOfBoundsException();
-    		for (int i=index; i<size-1; i++)
-    			this.arr[i] = this.arr[i+1];
-    		this.arr[this.size-1] = null;
-    		this.size--;
+        for (int i=index; i<size-1; i++)
+            this.arr[i] = this.arr[i+1];
+        this.arr[this.size-1] = null;
+        this.size--;
     }
     
     //Removes and returns the last element 
     public Object pop() {
-    		if (this.size == 0)
+        if (this.size == 0)
             throw new ArrayIndexOutOfBoundsException();
-    		Object endVal = this.arr[this.size-1];
-    		delete(this.size-1);
-    		if (this.size <= (int) 0.25*this.capacity)
-    			resize((int) 0.5*this.capacity);
-    		return endVal;
+        Object endVal = this.arr[this.size-1];
+        delete(this.size-1);
+        if (this.size <= (int) 0.25*this.capacity)
+            resize((int) 0.5*this.capacity);
+        return endVal;
     }
     
     //Removes each index holding the item
     public void remove(Object item) {
-    		for (int i=0; i<this.size; i++) {
-    			if (this.arr[i].equals(item)) {
-    				delete(i);
-    				i--;
-    			}
-    		}
+        for (int i=0; i<this.size; i++) {
+            if (this.arr[i].equals(item)) {
+                delete(i);
+                i--;
+            }
+        }
     }
     
     //Looks for item and returns index of first occurrence
     //Returns -1 if not found
     public int find(Object item) {
-    		for (int i=0; i<this.size; i++) {
-    			if (this.arr[i].equals(item)) 
-    				return i;
-    		}
-    		return -1;	
+        for (int i=0; i<this.size; i++) {
+            if (this.arr[i].equals(item)) 
+                return i;
+        }
+        return -1;	
     }
     
     //Handles array resizing
